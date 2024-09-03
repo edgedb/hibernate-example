fc35339c24c48a66e061c7e5c9fbada9b66282c8
Aljaž Mur Eržen <aljaz@edgedb.com>
2024-09-03 18:25:21 +0200

    Step 3: export data with pg_dump

---
+++ dump.sql
@@ -0,0 +1,194 @@
+-- Generated with:
+--
+--   pg_dump postgres://postgres:pass1234@localhost:5432/postgres \
+--     --inserts --column-inserts --data-only > dump.sql
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.4 (Debian 16.4-1.pgdg120+1)
+-- Dumped by pg_dump version 16.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: Position; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public."Position" (id, x, y) VALUES ('4357c4fb-0ca0-4af2-afe1-064d92389366', 0, -8);
+INSERT INTO public."Position" (id, x, y) VALUES ('87302ae6-220e-43c6-903d-1ba0b1e701a1', -30, -25);
+INSERT INTO public."Position" (id, x, y) VALUES ('f627901d-4fce-4200-9faa-ac58cb0dbd20', 24, 11);
+INSERT INTO public."Position" (id, x, y) VALUES ('5c2bca73-e41b-4f29-96f7-65f81f0fdd7a', -5, -3);
+INSERT INTO public."Position" (id, x, y) VALUES ('c24b1c8f-f690-4c2c-b852-301ba52d2a83', -3, -3);
+INSERT INTO public."Position" (id, x, y) VALUES ('6e383c19-654a-41ed-807f-f18161e7be86', 2, -2);
+INSERT INTO public."Position" (id, x, y) VALUES ('adcb6074-9ff8-4703-b32d-5e08c0c02cda', -3, -11);
+INSERT INTO public."Position" (id, x, y) VALUES ('02ba353b-3ec1-49c9-ab90-815e2dc6391a', -31, 15);
+INSERT INTO public."Position" (id, x, y) VALUES ('decd2b67-bf3f-428e-b01d-6e6538110455', -8, -11);
+INSERT INTO public."Position" (id, x, y) VALUES ('fbbc5e87-8310-4bbf-a5e6-629f886c7278', 29, 16);
+INSERT INTO public."Position" (id, x, y) VALUES ('ff40a59b-cd3d-4192-933e-b36af0b88a9c', 21, -12);
+INSERT INTO public."Position" (id, x, y) VALUES ('92656a9b-9766-45bc-9216-9713ceb0e1d2', 3, -27);
+INSERT INTO public."Position" (id, x, y) VALUES ('a058d8ca-e538-4add-ab0a-d7401ce345ce', 4, -16);
+INSERT INTO public."Position" (id, x, y) VALUES ('70903ee3-c8b4-4c4a-af22-4e686bdbf94e', -9, -13);
+INSERT INTO public."Position" (id, x, y) VALUES ('9e16f28f-7106-4455-bae1-145e1dadea9e', 39, -10);
+INSERT INTO public."Position" (id, x, y) VALUES ('513b832e-1352-4e07-9444-94739847ab7f', 5, 10);
+INSERT INTO public."Position" (id, x, y) VALUES ('ac76ee59-79d1-4003-8e89-ce9fdc4592ed', -10, 0);
+INSERT INTO public."Position" (id, x, y) VALUES ('b1429072-819a-41ef-8dc8-14465c1e8c91', -15, -19);
+INSERT INTO public."Position" (id, x, y) VALUES ('ffb279f9-306b-4f79-83ce-66fb4e2b23af', -25, 24);
+INSERT INTO public."Position" (id, x, y) VALUES ('2c4f83ef-5d57-42cf-abdf-2a5f2ac2bc04', -26, -19);
+INSERT INTO public."Position" (id, x, y) VALUES ('92d0d1cf-31d8-4626-8309-172302bc9c0c', 11, 0);
+INSERT INTO public."Position" (id, x, y) VALUES ('dbcd1db2-4fb9-4649-917e-dd93e28e0179', -5, -6);
+INSERT INTO public."Position" (id, x, y) VALUES ('cb8cbeb9-22f7-4b48-897b-edd40abbb8f8', 32, -33);
+INSERT INTO public."Position" (id, x, y) VALUES ('17406446-5e8d-4170-9568-f8121e42f973', -1, -41);
+INSERT INTO public."Position" (id, x, y) VALUES ('620fd934-5feb-45d8-b6b2-35d75ac0852e', -36, -5);
+INSERT INTO public."Position" (id, x, y) VALUES ('3a5d183e-1256-45eb-8cf6-d529d104535a', -9, 27);
+INSERT INTO public."Position" (id, x, y) VALUES ('ba327853-173a-4d1c-a873-90739269cb04', 3, -5);
+INSERT INTO public."Position" (id, x, y) VALUES ('d69f6ce3-d8ca-410b-9d07-b995868e30d8', -19, -12);
+INSERT INTO public."Position" (id, x, y) VALUES ('ff03ad41-05e1-419e-867e-60e0acbfc63f', -5, 2);
+INSERT INTO public."Position" (id, x, y) VALUES ('9a046266-6f26-4ea9-9927-517ed0b6fd05', -21, -34);
+INSERT INTO public."Position" (id, x, y) VALUES ('9d6d4c79-e8d1-4a23-aaa3-dcbd4fca6da5', -22, 18);
+INSERT INTO public."Position" (id, x, y) VALUES ('d8e2dcaa-8748-466b-8228-3f943c29793a', -27, 19);
+INSERT INTO public."Position" (id, x, y) VALUES ('474369fc-303f-4be5-aef3-9572d76f5a75', -21, -25);
+INSERT INTO public."Position" (id, x, y) VALUES ('014e11f5-c557-4544-a848-20fd85e00db1', -17, 7);
+INSERT INTO public."Position" (id, x, y) VALUES ('7f215d6a-ef7d-49f0-9740-ad422875fdd4', -19, -36);
+INSERT INTO public."Position" (id, x, y) VALUES ('776558d6-7f64-4743-8a4d-85ad258d65b8', -7, -8);
+INSERT INTO public."Position" (id, x, y) VALUES ('fa55bb8f-e865-4850-9c2c-8d69da9594f8', 17, 21);
+INSERT INTO public."Position" (id, x, y) VALUES ('976a5b8f-2e92-4416-b82d-7ece536aaaef', 27, -10);
+INSERT INTO public."Position" (id, x, y) VALUES ('fb1c0301-e2d1-4c57-8f63-5db00ee8dc39', -18, 14);
+INSERT INTO public."Position" (id, x, y) VALUES ('1dfc7ebe-5e5d-496b-ab47-d401d74bc824', -19, 4);
+INSERT INTO public."Position" (id, x, y) VALUES ('d58e9e1d-7f9e-4bf1-ae7b-4fef858987b1', 20, -24);
+INSERT INTO public."Position" (id, x, y) VALUES ('155c60c5-80ae-4366-9d6e-435e4260ce38', -11, -7);
+INSERT INTO public."Position" (id, x, y) VALUES ('85c261db-5149-488c-be10-852ae3653c9e', -7, -22);
+INSERT INTO public."Position" (id, x, y) VALUES ('cc076938-6d5d-4774-9763-6d605220231a', 30, 8);
+INSERT INTO public."Position" (id, x, y) VALUES ('60c8688e-8c8e-4b37-90e1-daf1c8869ea5', -18, 18);
+INSERT INTO public."Position" (id, x, y) VALUES ('4217a27f-102b-41b2-bf14-3b44b75386e0', -13, 29);
+INSERT INTO public."Position" (id, x, y) VALUES ('e17b2185-ab50-4fcf-8d5a-be3e3f3f000f', 5, -18);
+INSERT INTO public."Position" (id, x, y) VALUES ('896c658e-c0f5-4c4e-8f71-64cb4a76fa5b', -25, -12);
+INSERT INTO public."Position" (id, x, y) VALUES ('4352087a-c2ce-4060-ab30-a6dfc41b705d', 18, 14);
+INSERT INTO public."Position" (id, x, y) VALUES ('81c3861b-ddca-47a0-8e29-78283769b1d8', -22, 3);
+INSERT INTO public."Position" (id, x, y) VALUES ('233922c1-a05e-49aa-9711-19beadb7595a', 8, -4);
+INSERT INTO public."Position" (id, x, y) VALUES ('2da13af6-12b2-407e-b509-9c455d1e38fa', -17, -4);
+INSERT INTO public."Position" (id, x, y) VALUES ('dfb1a1b8-c4af-4a3c-a242-ed0645a457a2', 1, 28);
+INSERT INTO public."Position" (id, x, y) VALUES ('be19e53a-c1f9-42bf-8d2e-807a82020f59', 1, 37);
+
+
+--
+-- Data for Name: Carrot; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public."Carrot" (id, position_id) VALUES ('20485f67-945a-4b0e-9fbc-c80e6d62e670', '87302ae6-220e-43c6-903d-1ba0b1e701a1');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('9812c7ff-a68b-441b-a602-aa96ddc4fee9', 'f627901d-4fce-4200-9faa-ac58cb0dbd20');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('44742072-29a7-4bc9-91eb-0c77d46e1f3f', 'fbbc5e87-8310-4bbf-a5e6-629f886c7278');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('a0b2061f-a471-400e-bc51-3b47d2fcecc4', '92656a9b-9766-45bc-9216-9713ceb0e1d2');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('86266823-d8c5-4a39-982d-10fad0f4c4b5', '9e16f28f-7106-4455-bae1-145e1dadea9e');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('9bb2614c-f235-44e6-b117-bf2508ef5fd3', 'b1429072-819a-41ef-8dc8-14465c1e8c91');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('e2385aa6-e303-424b-9cb1-8e041c8d907d', '2c4f83ef-5d57-42cf-abdf-2a5f2ac2bc04');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('f814ac85-af73-49d3-87b3-724722041c96', '620fd934-5feb-45d8-b6b2-35d75ac0852e');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('cbc5fa00-548b-4cde-b9a1-a62125abfbb5', 'd69f6ce3-d8ca-410b-9d07-b995868e30d8');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('5b53d4e6-88b4-4178-a4b5-eba389bc8c4f', '9a046266-6f26-4ea9-9927-517ed0b6fd05');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('15cc1322-acb7-43d3-a7db-4426c0edcf26', '474369fc-303f-4be5-aef3-9572d76f5a75');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('ed8f50f7-a5b4-417c-9012-96f613b24087', '976a5b8f-2e92-4416-b82d-7ece536aaaef');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('80bb09e3-11a2-46ae-98c0-12f99138061c', 'fb1c0301-e2d1-4c57-8f63-5db00ee8dc39');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('02aea592-7c5f-425b-9ae5-2e217506c5df', '1dfc7ebe-5e5d-496b-ab47-d401d74bc824');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('0c998784-c0b7-4ec2-9964-870ff4952f72', 'd58e9e1d-7f9e-4bf1-ae7b-4fef858987b1');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('765f65c3-d90c-4795-bfd5-1ada7705a9b0', '85c261db-5149-488c-be10-852ae3653c9e');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('2536a642-7dda-4a9c-9355-87ee3097166a', 'cc076938-6d5d-4774-9763-6d605220231a');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('3bb8c5a4-a090-4ee4-be6c-2a2796ee70f1', '60c8688e-8c8e-4b37-90e1-daf1c8869ea5');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('ea60e5f8-6207-44ec-bfc7-93c5add2d69a', '4217a27f-102b-41b2-bf14-3b44b75386e0');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('aabac7f8-2e7b-4d91-ab24-0a7273099d4c', 'e17b2185-ab50-4fcf-8d5a-be3e3f3f000f');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('58674c5e-3631-4b0f-bbdf-de177537af27', '896c658e-c0f5-4c4e-8f71-64cb4a76fa5b');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('38bbcdeb-d044-411f-b849-0f525b39b610', '81c3861b-ddca-47a0-8e29-78283769b1d8');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('7da120cf-e6b6-4484-a99a-067533f5df2b', '2da13af6-12b2-407e-b509-9c455d1e38fa');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('cf347699-c228-436d-abbc-4e32fb2e877a', 'be19e53a-c1f9-42bf-8d2e-807a82020f59');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('36730942-44f9-4598-aabb-ceef38d95a31', 'ff40a59b-cd3d-4192-933e-b36af0b88a9c');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('4897c9b0-b69e-4cee-9ea7-6c2e12cad42a', '92d0d1cf-31d8-4626-8309-172302bc9c0c');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('89c3d221-621d-4654-a861-2ae5c7645218', 'cb8cbeb9-22f7-4b48-897b-edd40abbb8f8');
+INSERT INTO public."Carrot" (id, position_id) VALUES ('f6435598-a07b-4397-9d99-d64a74b163ed', 'd8e2dcaa-8748-466b-8228-3f943c29793a');
+
+
+--
+-- Data for Name: Figure; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('b5e6012c-69dc-48c4-8758-be5bbfd74805', 'Rodrigo', 0, 1, '4357c4fb-0ca0-4af2-afe1-064d92389366');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('1f1abae1-4f22-4e8a-9adb-8ffb914c3d0b', 'Finneas', 0, 0, '5c2bca73-e41b-4f29-96f7-65f81f0fdd7a');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('5edaf202-92a5-4a73-85c8-b8afb9bc4911', 'Ridge', 0, 2, 'c24b1c8f-f690-4c2c-b852-301ba52d2a83');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('9d7021c3-e3d2-4e7a-9993-b8d92bdc6a3f', 'Fermina', 0, 0, '6e383c19-654a-41ed-807f-f18161e7be86');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('9f407672-9773-4999-a381-9f5de2fe3a7b', 'Fidela', 0, 0, '02ba353b-3ec1-49c9-ab90-815e2dc6391a');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('dac497e3-7e77-4881-83c1-9af67b841400', 'Feba', 0, 0, 'adcb6074-9ff8-4703-b32d-5e08c0c02cda');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('94b9feac-a931-4331-88db-ed50264f4c80', 'Reece', 0, 3, 'decd2b67-bf3f-428e-b01d-6e6538110455');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('eb24b6b9-8bb8-4a35-a07a-ef63d9215c21', 'Freyr', 0, 0, 'a058d8ca-e538-4add-ab0a-d7401ce345ce');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('6fc3b889-aef8-47e6-ad96-ee36ee14d007', 'Francesca', 0, 0, '70903ee3-c8b4-4c4a-af22-4e686bdbf94e');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('90f95f62-a9d1-460f-bf42-60826c29ee21', 'Rivka', 1, 7, 'ac76ee59-79d1-4003-8e89-ce9fdc4592ed');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('130982e2-994c-46e5-b829-e0b89e8ba26a', 'Reese', 1, 3, NULL);
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('7f3603ab-4ca6-455c-b5a5-5886e84261b7', 'Fang', 0, 0, 'ffb279f9-306b-4f79-83ce-66fb4e2b23af');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('64131732-dc11-45f5-a616-1f2d99598d1a', 'Frye', 1, 5, '513b832e-1352-4e07-9444-94739847ab7f');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('366648d2-c2e6-4d8f-b27b-e8e3edef20cd', 'Rosemarie', 0, 4, 'dbcd1db2-4fb9-4649-917e-dd93e28e0179');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('80a14529-e929-487c-8af2-3dc2ce87c24b', 'Francisca', 0, 0, '17406446-5e8d-4170-9568-f8121e42f973');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('56f7bb13-a7eb-49f3-8cdd-ff1e7b49637b', 'Feba', 0, 0, '3a5d183e-1256-45eb-8cf6-d529d104535a');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('9f34c7bd-d502-4605-8ccf-e85609d2eed7', 'Rumaysa', 0, 6, 'ba327853-173a-4d1c-a873-90739269cb04');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('f5d803ba-343c-4545-be2a-2ae12322cf23', 'Farnell', 0, 0, '9d6d4c79-e8d1-4a23-aaa3-dcbd4fca6da5');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('cb8b23b6-e4fc-4ff7-b091-a5fbeca60c4b', 'Roland', 3, 47, NULL);
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('16a1d172-5b0b-4066-978a-c9256215a725', 'Falkner', 2, 10, 'ff03ad41-05e1-419e-867e-60e0acbfc63f');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('7098fe5e-724c-4a24-8857-8eccb76d5cc6', 'Farran', 0, 0, '014e11f5-c557-4544-a848-20fd85e00db1');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('d34bb029-ec1c-4f2d-85f8-7b0ec2d7264d', 'Faustino', 0, 0, '7f215d6a-ef7d-49f0-9740-ad422875fdd4');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('e439010a-a378-4ebe-9e18-ff2adee385b4', 'Fujio', 0, 0, '776558d6-7f64-4743-8a4d-85ad258d65b8');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('d96dcbfa-fd02-4d7d-8e68-77b70965c1ab', 'Fatimah', 0, 0, 'fa55bb8f-e865-4850-9c2c-8d69da9594f8');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('51913450-cff5-4aed-b90b-5a6d1e483089', 'Fayaz', 0, 0, '155c60c5-80ae-4366-9d6e-435e4260ce38');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('767e3301-1ff5-4566-8652-7d9482e3c2e8', 'Finneas', 0, 0, '4352087a-c2ce-4060-ab30-a6dfc41b705d');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('52b7c683-631b-4e0c-84ef-5f3f1e01d151', 'Forrest', 0, 0, '233922c1-a05e-49aa-9711-19beadb7595a');
+INSERT INTO public."Figure" (id, name, score, stat_moves, position_id) VALUES ('4122eda3-842f-463f-8bf0-532bf4c75f45', 'Frederica', 0, 0, 'dfb1a1b8-c4af-4a3c-a242-ed0645a457a2');
+
+
+--
+-- Data for Name: Fox; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public."Fox" (figure_id) VALUES ('16a1d172-5b0b-4066-978a-c9256215a725');
+INSERT INTO public."Fox" (figure_id) VALUES ('1f1abae1-4f22-4e8a-9adb-8ffb914c3d0b');
+INSERT INTO public."Fox" (figure_id) VALUES ('9d7021c3-e3d2-4e7a-9993-b8d92bdc6a3f');
+INSERT INTO public."Fox" (figure_id) VALUES ('9f407672-9773-4999-a381-9f5de2fe3a7b');
+INSERT INTO public."Fox" (figure_id) VALUES ('eb24b6b9-8bb8-4a35-a07a-ef63d9215c21');
+INSERT INTO public."Fox" (figure_id) VALUES ('6fc3b889-aef8-47e6-ad96-ee36ee14d007');
+INSERT INTO public."Fox" (figure_id) VALUES ('64131732-dc11-45f5-a616-1f2d99598d1a');
+INSERT INTO public."Fox" (figure_id) VALUES ('7f3603ab-4ca6-455c-b5a5-5886e84261b7');
+INSERT INTO public."Fox" (figure_id) VALUES ('56f7bb13-a7eb-49f3-8cdd-ff1e7b49637b');
+INSERT INTO public."Fox" (figure_id) VALUES ('f5d803ba-343c-4545-be2a-2ae12322cf23');
+INSERT INTO public."Fox" (figure_id) VALUES ('7098fe5e-724c-4a24-8857-8eccb76d5cc6');
+INSERT INTO public."Fox" (figure_id) VALUES ('d34bb029-ec1c-4f2d-85f8-7b0ec2d7264d');
+INSERT INTO public."Fox" (figure_id) VALUES ('e439010a-a378-4ebe-9e18-ff2adee385b4');
+INSERT INTO public."Fox" (figure_id) VALUES ('d96dcbfa-fd02-4d7d-8e68-77b70965c1ab');
+INSERT INTO public."Fox" (figure_id) VALUES ('51913450-cff5-4aed-b90b-5a6d1e483089');
+INSERT INTO public."Fox" (figure_id) VALUES ('767e3301-1ff5-4566-8652-7d9482e3c2e8');
+INSERT INTO public."Fox" (figure_id) VALUES ('52b7c683-631b-4e0c-84ef-5f3f1e01d151');
+INSERT INTO public."Fox" (figure_id) VALUES ('4122eda3-842f-463f-8bf0-532bf4c75f45');
+INSERT INTO public."Fox" (figure_id) VALUES ('dac497e3-7e77-4881-83c1-9af67b841400');
+INSERT INTO public."Fox" (figure_id) VALUES ('80a14529-e929-487c-8af2-3dc2ce87c24b');
+
+
+--
+-- Data for Name: Rabbit; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (14, 'b5e6012c-69dc-48c4-8758-be5bbfd74805');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (13, '5edaf202-92a5-4a73-85c8-b8afb9bc4911');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (12, '94b9feac-a931-4331-88db-ed50264f4c80');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (27, '130982e2-994c-46e5-b829-e0b89e8ba26a');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (23, '90f95f62-a9d1-460f-bf42-60826c29ee21');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (11, '366648d2-c2e6-4d8f-b27b-e8e3edef20cd');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (9, '9f34c7bd-d502-4605-8ccf-e85609d2eed7');
+INSERT INTO public."Rabbit" (nutrition, figure_id) VALUES (13, 'cb8b23b6-e4fc-4ff7-b091-a5fbeca60c4b');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
